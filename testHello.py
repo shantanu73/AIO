df61baea98ec232eef9f6c5e13584c0bd113285a
shantanu73 <shantanudhiman25@gmail.com>
2018-11-05 10:49:20 +0530

    Update testHello.py

--- testHello.py
+++ testHello.py
@@ -1,11 +1,26 @@
 from aiohttp import web
 import json
+from datetime import datetime
+import random, string
 
-async def handle(request):
-    response_obj = { 'status' : 'success' }
-    return web.Response(text=json.dumps(response_obj))
+
+
+async def new_register(request):
+    try:
+        agent_id = request.query['agent_id']
+        system_id = request.query['system_id']
+        version = request.query['version']
+        handler_uuid = ''.join(random.choice(string.ascii_lowercase + string.digits) for i in range(16))
+        registered_time = datetime.datetime.now().strftime('%H:%M:%S')
+        
+        
+        response_obj = { 'handler_uuid' : handler_uuid }
+        return web.Response(text=json.dumps(response_obj), status=200)
+    except Exception as e:
+        response_obj = { 'status' : 'failed', 'reason': str(e) }        
+        return web.Response(text=json.dumps(response_obj), status=500)
 
 app = web.Application()
-app.router.add_get('/', handle)
+app.router.add_get('/register', new_register)
 
 web.run_app(app)
