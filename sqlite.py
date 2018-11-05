6a5b91bda868ae533b0a3bd189ee91729242fc8b
shantanu73 <shantanudhiman25@gmail.com>
2018-11-05 14:11:36 +0530

    Update sqlite.py

--- sqlite.py
+++ sqlite.py
@@ -13,10 +13,9 @@ class User(BaseModel):
 
 print('\n\nHI\n\n')
 User.create_table(True)
-u_obj = User()
  
-u_in = (u_obj.insert(username='a3'))
+u_in = (User.insert(username='a3'))
 u_in.execute()
-user_list = (u_obj.select(u_obj))
+user_list = (User.select(User))
 for u in user_list:
     print(u.username)
