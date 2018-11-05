99c25a38e7669bc1acc0ae4db6f3544cdd0ae516
shantanu73 <shantanudhiman25@gmail.com>
2018-11-05 12:31:10 +0530

    Update peewee_db.py

--- peewee_db.py
+++ peewee_db.py
@@ -4,5 +4,29 @@ import logging
 from peewee_async import Manager, PostgresqlDatabase
 
 loop = asyncio.new_event_loop() # Note: custom loop!
-database = PostgresqlDatabase('test')
+database = PostgresqlDatabase('Handler_db')
+
 objects = Manager(database, loop=loop)
+
+class Handler(peewee.Model):
+    handler_uuid = peewee.CharField(max_length=40, unique=True)
+    agent_id = peewee.CharField(max_length=40)
+    system_id = peewee.CharField(max_length=40)
+    vesion = peewee.CharField(max_length=40)
+    registered_time = peewee.CharField(max_length=40)
+
+    class Meta:
+        database = database
+        
+# creating table for above block
+Handler.create_table(True)
+query = Handler
+       .insert_from(
+           Handler.handler_uuid, Handler.agent_id, Handler.system_id, Handler.vesion, Handler.registered_time ),
+           fields=['a', 'b', 'c', 'd', 'e'])
+query.execute()
+query1 = Handler
+       .select(
+           Handler.handler_uuid, Handler.agent_id, Handler.system_id, Handler.vesion, Handler.registered_time )
+           )
+query1.execute()
