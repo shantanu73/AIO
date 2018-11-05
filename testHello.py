53f21a91adc910e12f4f25506c0a6d10633095c6
shantanu73 <shantanudhiman25@gmail.com>
2018-11-06 02:38:37 +0530

    Update testHello.py

--- testHello.py
+++ testHello.py
@@ -1,7 +1,19 @@
+'''
+Filename : handler_app.py
+
+Author : Shantanu Dhiman
+
+Desciption : 
+    Handle '/register' request.
+    Set values for agent_id, system_id, version, handler_uuid & registered_time, 
+    then put it into Handler table. If operation successful, then, return handler_uuid
+    as response, otherwise return failure message.
+    Prints all rows of the table 'Handler'.
+'''
+
 from aiohttp import web
 import json
 import random, string
-import time
 import datetime
 from peewee import *
 
@@ -12,30 +24,57 @@ class BaseModel(Model):
         database = db
 
 class Handler(BaseModel):
+    '''
+    This is a class for table 'Handler'. 
+      
+    Attributes: 
+        handler_uuid (string) 
+        agent_id (string)
+        system_id (string)
+        version (string)
+        registered_time (string) 
+    '''
     handler_uuid = CharField(primary_key=True)
     agent_id = CharField(max_length=40)
     system_id = CharField(max_length=40)
-    vesion = CharField(max_length=40)
+    version = CharField(max_length=40)
     registered_time = CharField(max_length=40)
 
 Handler.create_table(True)
     
 async def new_register(request):
+    '''
+    Handle '/register' request.
+    Set values for agent_id, system_id, version, handler_uuid & registered_time, 
+    then put it into Handler table. If operation successful, then, return handler_uuid
+    as response, otherwise return failure message.
+    Prints all rows of the table 'Handler'.
+  
+    Parameters: 
+    request (): request url which contains agent_id, system_id & version.
+  
+    Returns: 
+    response_obj (json): handler_uuid if success otherwise failure status. 
+    '''
     try:
+        # set values for agent_id, system_id, vesion, handler_uuid and registered_time
         agent_id = request.query['agent_id']
         system_id = request.query['system_id']
-        vesion = request.query['vesion']
-        handler_uuid = ''.join(random.choice(string.ascii_lowercase + string.digits) for i in range(16))
+        version = request.query['vesion']
+        handler_uuid = ''.join(random.choice(string.ascii_lowercase + string.digits) for i in range(32))
         registered_time = datetime.datetime.now().strftime('%H:%M:%S')
         
-        handler_data = (Handler.insert(handler_uuid=handler_uuid,agent_id=agent_id,system_id=system_id,vesion=vesion,registered_time=registered_time))
+        # query to insert values into database
+        handler_data = (Handler.insert(handler_uuid=handler_uuid,agent_id=agent_id,system_id=system_id,version=version,registered_time=registered_time))
         handler_data.execute()
+        
+        # query to fetch all rows from database  
         handler_list = (Handler.select(Handler))
         for row in handler_list:
             print(row.handler_uuid)
             print(row.agent_id)
             print(row.system_id)
-            print(row.vesion)
+            print(row.version)
             print(row.registered_time)
             
         response_obj = { 'handler_uuid' : handler_uuid }
