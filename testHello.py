e5244ed27e4f4af5b77ae231a48b6363dae8fadf
shantanu73 <shantanudhiman25@gmail.com>
2018-11-22 17:07:28 +0530

    Update testHello.py

--- testHello.py
+++ testHello.py
@@ -1,92 +1,50 @@
-'''
-Filename : handler_app.py
-
-Author : Shantanu Dhiman
-
-Desciption : 
-    Handle '/register' request.
-    Set values for agent_id, system_id, version, handler_uuid & registered_time, 
-    then put it into Handler table. If operation successful, then, return handler_uuid
-    as response, otherwise return failure message.
-    Prints all rows of the table 'Handler'.
-'''
-
-from aiohttp import web
 import json
+from datetime import datetime
 import random, string
+import time
 import datetime
 from peewee import *
 
-db = SqliteDatabase('Handler_db.db')
+db = SqliteDatabase('my_app.db')
 
 class BaseModel(Model):
     class Meta:
         database = db
 
 class Handler(BaseModel):
-    '''
-    This is a class for table 'Handler'. 
-      
-    Attributes: 
-        handler_uuid (string) 
-        agent_id (string)
-        system_id (string)
-        version (string)
-        registered_time (string) 
-    '''
     handler_uuid = CharField(primary_key=True)
     agent_id = CharField(max_length=40)
     system_id = CharField(max_length=40)
-    version = CharField(max_length=40)
+    vesion = CharField(max_length=40)
     registered_time = CharField(max_length=40)
 
 Handler.create_table(True)
     
 async def new_register(request):
-    '''
-    Handle '/register' request.
-    Set values for agent_id, system_id, version, handler_uuid & registered_time, 
-    then put it into Handler table. If operation successful, then, return handler_uuid
-    as response, otherwise return failure message.
-    Prints all rows of the table 'Handler'.
-  
-    Parameters: 
-    request (): request url which contains agent_id, system_id & version.
-  
-    Returns: 
-    response_obj (json): handler_uuid if success otherwise failure status. 
-    '''
     try:
-        # set values for agent_id, system_id, vesion, handler_uuid and registered_time
-        print(type(request))
         agent_id = request.query['agent_id']
         system_id = request.query['system_id']
-        version = request.query['vesion']
-        handler_uuid = ''.join(random.choice(string.ascii_lowercase + string.digits) for i in range(32))
-        registered_time = datetime.datetime.now().strftime('%H:%M:%S')
+        vesion = request.query['vesion']
+        handler_uuid = ''.join(random.choice(string.ascii_lowercase + string.digits) for i in range(16))
+        registered_time = 'current date'#datetime.now().strftime('%H:%M:%S')
         
-        # query to insert values into database
-        handler_data = (Handler.insert(handler_uuid=handler_uuid,agent_id=agent_id,system_id=system_id,version=version,registered_time=registered_time))
+        handler_data = (Handler.insert(handler_uuid=handler_uuid,agent_id=agent_id,system_id=system_id,vesion=vesion,registered_time=registered_time))
         handler_data.execute()
-        
-        # query to fetch all rows from database  
         handler_list = (Handler.select(Handler))
-        '''
         for row in handler_list:
             print(row.handler_uuid)
             print(row.agent_id)
             print(row.system_id)
-            print(row.version)
+            print(row.vesion)
             print(row.registered_time)
-        '''
-        response_obj = { 'handler_uuid' : handler_uuid }
-        return web.Response(text=json.dumps(response_obj))
+            
+        response_obj = { 'handler_uuid' : handler_uuid , 'registered_time' : registered_time }
+        return web.Response(text=json.dumps(response_obj), status=200)
     except Exception as e:
         response_obj = { 'status' : 'failed', 'reason': str(e) }        
-        return web.Response(text=json.dumps(response_obj))
+        return web.Response(text=json.dumps(response_obj), status=500)
 
 app = web.Application()
 app.router.add_get('/register', new_register)
-app.router.add_get('/number', number)
 
 web.run_app(app)
