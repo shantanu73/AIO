7a63a93794167b0f1460cbe6f045b49fe3120dca
shantanu73 <shantanudhiman25@gmail.com>
2018-11-22 17:17:48 +0530

    Create test1.py

---
+++ test1.py
@@ -0,0 +1,21 @@
+from aiohttp import web
+import json
+from datetime import datetime
+import random, string
+import time
+import datetime
+    
+async def new_register(request):
+    try:
+        agent_id = request.query['agent_id']
+        print(agent_id)
+        response_obj = { 'agent_id' : agent_id }
+        return web.Response(text=json.dumps(response_obj), status=200)
+    except Exception as e:
+        response_obj = { 'status' : 'failed', 'reason': str(e) }        
+        return web.Response(text=json.dumps(response_obj), status=500)
+
+app = web.Application()
+app.router.add_get('/register', new_register)
+
+web.run_app(app)
